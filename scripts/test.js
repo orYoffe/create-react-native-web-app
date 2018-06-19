815d7194cad7e5126d03e1ec5e09b2fd6269549e
orYoffe <ragiop07@gmail.com>
2018-06-19 14:37:47 +0200

    initial commit - merged cra and crna

---
+++ scripts/test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const switchSnapshots = require('./switchSnapshots');
+
+// Do this as the first thing so that any code reading it knows the right env.
+const isNative = process.argv.includes('--n');
+process.env.BABEL_ENV = isNative ? 'test' : 'test-web';
+process.env.NODE_ENV = 'test';
+process.env.PUBLIC_URL = '';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Ensure environment variables are read.
+require('../config/env');
+
+const jest = require('jest');
+
+const argv = isNative ? process.argv.slice(3) : process.argv.slice(2);
+
+switchSnapshots(isNative);
+
+// Watch unless on CI or in coverage mode
+if (!process.env.CI && argv.indexOf('--coverage') < 0) {
+  argv.push('--watch');
+}
+
+
+jest.run(argv);
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import logo from './logo.png';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo}/>
+        <Text style={styles.title}>Create React Native Web App</Text>
+        <Text>Open up src/App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default App;
