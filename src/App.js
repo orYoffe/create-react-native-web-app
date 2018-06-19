8ec684537f4295d9e19ab99ca9f0a6cc216a1946
orYoffe <ragiop07@gmail.com>
2018-06-19 16:24:52 +0200

    added animation and button example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TouchableHighlight,
+  Animated,
+  Easing,
+} from 'react-native';
 import logo from './logo.png';
 
 class App extends Component {
+  state = {
+    spinValue: new Animated.Value(0),
+  }
+
+  onClick = () => {
+    const wasRotated = this.state.spinValue._value === 1;
+    Animated.timing(
+      this.state.spinValue,
+      {
+        toValue: wasRotated ? 0 : 1,
+        duration: 250,
+        easing: Easing.linear
+      }
+    ).start()
+  }
+
   render() {
+    const spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
+
     return (
       <View style={styles.container}>
-        <Image source={logo} style={styles.logo}/>
+        <Animated.Image source={logo} style={[styles.logo, { transform: [{rotate: spin}] }]}/>
         <Text style={styles.title}>Create React Native Web App</Text>
         <Text>Open up src/App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        {Platform.OS !== 'web' && <Text>Shake your phone to open the developer menu.</Text>}
+        <TouchableHighlight
+          onPress={this.onClick}
+          style={styles.button}
+          underlayColor={'#0A84D0'}
+        >
+          <Text style={styles.buttonText}>Rotate Logo</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -28,6 +64,18 @@ const styles = StyleSheet.create({
     height: 300,
   },
   title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  button: {
+    borderRadius: 3,
+    padding: 20,
+    marginVertical: 10,
+    marginTop: 10,
+    backgroundColor: '#1B95E0',
+  },
+  buttonText: {
+    color: '#fff',
     fontWeight: 'bold',
     fontSize: 16,
   },
