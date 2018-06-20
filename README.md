4c117a1e9e414f55784a6956adc8103a828c3be1
orYoffe <ragiop07@gmail.com>
2018-06-20 14:47:20 +0200

    build cli and readme

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
-# create-react-native-web-app
-React Native Web CLI
+# React Native Web CLI
+### create-react-native-web-app
+### A simple CLI tool to start your React Native Web project to develop same app for IOS Android and Web.
+
+<p align="center" >
+<img width="300"  src="https://raw.githubusercontent.com/VISI-ONE/create-react-native-web-app/master/template/src/logo.png">
+</p>
+
+## Installation
+
+- Android - Install [adb (Android Debug Bridge)](https://developer.android.com/studio/releases/platform-tools.html)
+- IOS - Xcode and an apple device required
+
+
+- Install the package globally and run it with your name of choice
+
+```sh
+# Install package
+$ npm install -g create-react-native-web-app
+
+# Run create-react-native-web-app <project-directory>
+$ create-react-native-web-app myApp
+
+# cd into your <project-directory>
+$ cd myApp
+
+```
+
+- Optional - Install Xcode and Android studio and follow the react native instructions [under the "Building Projects with Native Code" tab](http://facebook.github.io/react-native/docs/getting-started.html)
+
+## Running development
+
+```sh
+# Web
+$ npm run web
+
+# IOS (simulator)
+$ npm run ios
+
+# Android (connected device)
+$ npm run android
+```
+
+## Testing
+
+```sh
+# Web and Native
+$ npm run test
+
+# Web
+$ npm run test:web
+
+# Web watch mode
+$ npm run test:web-watch
+
+
+# Native
+$ npm run test:native
+
+# Native watch mode
+$ npm run test:native-watch
+
+
+# Coverage - web
+$ npm run coverage
+
+# Coverage - native
+$ npm run coverage:native
+```
+
+## Debugging
+
+Open dev menu:
+1. CMD+D (IOS) / CMD+M (Android)
+2. Press "Enable Live-Reload"
+
+[React native docs - debugging real devices guide](http://facebook.github.io/react-native/releases/0.49/docs/running-on-device.html)
+
+[React native docs - debugging guide](http://facebook.github.io/react-native/docs/debugging.html)
+
+[Network calls in the devtools](http://www.preslav.me/2017/03/26/debugging-network-calls-in-react-native-using-the-chrome-debugger/)
+
+## Build
+```sh
+# Web
+$ npm run build
+
+# Android - upgrade the current build version in `android/app/build.gradle` file (both the `versionCode` and the `versionName`)
+Example:
+versionCode 2
+versionName "1.1"
+
+# And then run the build
+$ cd android && ./gradlew assembleRelease
+
+# Open apk folder to find the release apk
+$ open ./android/app/build/outputs/apk
+```
+
+[React native docs - Android signed apk](http://facebook.github.io/react-native/releases/0.49/docs/signed-apk-android.html)
+
+[React native docs for IOS](http://facebook.github.io/react-native/releases/0.49/docs/running-on-device.html#building-your-app-for-production)
+
+
+# Contributors
+- Or Yoffe https://github.com/orYoffe
+
+## Resources
+- [React Native for Web (react-native-web)](https://github.com/necolas/react-native-web)
+- [React](https://reactjs.org/)
+- [React Native](http://facebook.github.io/react-native/)
--- cli.js
+++ cli.js
@@ -1,13 +1,119 @@
+const chalk = require('chalk');
+const commander = require('commander');
+const fs = require('fs-extra');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const packageJson = require('./package.json');
 
-const appName = process.argv[2];
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 4) {
+  console.error(
+    chalk.red(
+      'You are running Node ' +
+        currentNodeVersion +
+        '.\n' +
+        'Create React App requires Node 4 or higher. \n' +
+        'Please update your version of Node.'
+    )
+  );
+  process.exit(1);
+}
+
+const printCyan = text => console.log(`      ${chalk.cyan(text)}`);
+const printGreen = text => console.log(`      ${chalk.green(text)}`);
+
+let appName;
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')}`)
+  .action(name => {
+    appName = name;
+  })
+  .on('--help', () => {
+    console.log(`    Only ${chalk.green('<project-directory>')} is required.`);
+    console.log();
+    console.log(
+      `    If you have any problems, do not hesitate to file an issue:`
+    );
+    printCyan('https://github.com/VISI-ONE/create-react-native-web-app/issues/new');
+    console.log();
+  })
+  .parse(process.argv);
+
+
+let isYarnAvailable;
+try {
+  execSync('yarnpkg --version', { stdio: 'ignore' });
+  isYarnAvailable = true;
+} catch (e) {
+  isYarnAvailable = false;
+}
+
+
 if (appName) {
-  console.log('Building...');
-// TODO mkdir appName if doesn't exist
-// TODO copy files
-// TODO install deps
-// TODO print script commands with link to repo
+  printCyan(`⏳ Creating React Native Web App by the name of ${appName} ...`);
+  console.log();
 
-console.log('---------__dirname--------', __dirname);
+  // create folder appName and copy files
+  fs.ensureDirSync(appName);
+  fs.emptyDirSync(appName);
+
+  printCyan('✅ Created project folder.');
+  console.log();
+  const copy = fs.copySync(path.resolve(__dirname, 'template'), appName);
+
+  printCyan('✅ Added project files.');
+  console.log();
+
+  // install deps
+  printCyan('⏳ Installing project dependencies...');
+  console.log();
+  let command = `cd ${appName} && `;
+  let args;
+  if (isYarnAvailable) {
+    command += 'yarn';
+  } else {
+    command += 'npm i';
+  }
+
+  execSync(command,  { stdio: [0,1,2] });
+
+  printCyan('✅ Installed project dependencies.');
+  console.log();
+
+  // print script commands with info links
+  printGreen('✅ Done! 😁👍 Your project is ready for development.');
+  console.log();
+  const packageManagerRunCommand = isYarnAvailable ? 'yarn' : 'npm run';
+  console.log(`
+        ${chalk.magenta('*')} ${chalk.magenta('change directory to your new project')}
+        $ ${chalk.cyan(`cd ${appName}`)}
+
+        $ ${chalk.cyan('Then run the these commands to get started:')}
+
+        ${chalk.magenta('*')} ${chalk.magenta('To run development Web server')}
+        $ ${chalk.cyan(packageManagerRunCommand + ' web')}
+
+        ${chalk.magenta('*')} ${chalk.magenta('To run Android on connected device (after installing Android Debug Bridge "adb" - https://developer.android.com/studio/releases/platform-tools)')}
+        $ ${chalk.cyan(packageManagerRunCommand + ' android')}
+
+        ${chalk.magenta('*')} ${chalk.magenta('To run ios simulator (after installing Xcode - only on Apple devices)')}
+        $ ${chalk.cyan(packageManagerRunCommand + ' ios')}
+
+        ${chalk.magenta('*')} ${chalk.magenta('To run tests for Native and Web')}
+        $ ${chalk.cyan(packageManagerRunCommand + ' test')}
+
+        ${chalk.magenta('*')} ${chalk.magenta('To run build for Web')}
+        $ ${chalk.cyan(packageManagerRunCommand + ' build')}
+    `);
 } else {
-  console.log('In order to create a new project you must give a name as an argument. Example: create-rnw-app AppName');
+  console.error(
+    chalk.red('In order to create a new project you must give a name as an argument. '),
+    chalk.cyan('Example: create-react-native-web-app AppName')
+  );
+  process.exit(1);
 }
